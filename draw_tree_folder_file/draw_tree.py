5f28d88e45e27ae90490dfa2340d26386f7b1332
NgoTamAIRC <133946215+Moobbot2@users.noreply.github.com>
2024-04-09 10:19:14 +0700

    update draw tree v2

--- draw_tree_folder_file/draw_tree.py
+++ draw_tree_folder_file/draw_tree.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 
-def list_files(directory):
+def list_files(directory, output_file=None):
     # Function to check if a path is ignored by gitignore rules
     def is_ignored(path):
         gitignore_path = Path(path)
@@ -20,29 +20,40 @@ def list_files(directory):
         return False
 
     # Recursive function to list files in directory
-    def list_files_recursively(directory, indent=''):
+    def list_files_recursively(directory, indent='', output_file=None):
+        # Function to sort files and directories alphabetically
+        def sort_files_and_directories(files, directories):
+            return sorted(files), sorted(directories)
+        
         file_list = []
-        for root, dirs, files in os.walk(directory):
-            # Filter out ignored files and directories
-            dirs[:] = [d for d in dirs if not is_ignored(
-                os.path.join(root, d))]
-            files = [f for f in files if not is_ignored(os.path.join(root, f))]
-            # Add remaining files to the list
-            for f in files:
-                print(indent + '|- ' + f)
-                output_file.write(indent + '|- ' + f + '\n')
-            for d in dirs:
-                print(indent + '- ' + d)
-                output_file.write(indent + '- ' + d + '\n')
-                file_list.extend(list_files_recursively(
-                    os.path.join(root, d), indent + '  '))
+        # If output_file is None, use print to output
+        if output_file is None:
+            output_function = print
+        else:
+            output_function = output_file.write
+
+        items = os.listdir(directory)
+        files = [item for item in items if os.path.isfile(os.path.join(directory, item)) and not is_ignored(os.path.join(directory, item))]
+        dirs = [item for item in items if os.path.isdir(os.path.join(directory, item)) and not is_ignored(os.path.join(directory, item))]
+
+        files, dirs = sort_files_and_directories(files, dirs)
+
+        # Print files
+        for f in files:
+            output_function(indent + '|- ' + f + '\n')
+        # Print directories
+        for d in dirs:
+            output_function(indent + '- ' + d + '\n')
+            file_list.extend(list_files_recursively(
+                os.path.join(directory, d), indent + '  ', output_file=output_file))
         return file_list
 
-    return list_files_recursively(directory)
+    # Replace 'your_directory' with the directory you want to list files from
+    return list_files_recursively(directory, output_file=output_file)
 
 
 # Replace 'your_directory' with the directory you want to list files from
 directory = os.getcwd()
 with open('directory_tree.txt', 'w') as output_file:
     output_file.write(directory + '\n')
-    files = list_files(directory)
+    files = list_files(directory, output_file=output_file)
